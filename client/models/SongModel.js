b66679e9791b21a47c9b14ae969404beba488b64
Anil Pai <anilbpai@gmail.com>
2016-06-02 18:03:26 -0400

    Extra Credit : upvote/downvote

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,17 +1,40 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
   initialize: function(){
-    var playCnt = Number(localStorage.getItem(this.get("url"))) || 0;
-    this.set('playCount', playCnt);
+    var obj = JSON.parse(localStorage.getItem(this.get('url')));
+    this.set('playCount', obj ? Number(obj['playCount']): 0);
+    this.set('upCount', obj ? Number(obj['upCount']): 0);
+    this.set('downCount', obj ? Number(obj['downCount']): 0);
   },
+
+  incrUpCount : function(){
+    this.set('upCount', this.get('upCount')+1);
+    this.writeLocalStorage();
+  },
+
+  incrDownCount : function(){
+    this.set('downCount', this.get('downCount')+1);
+    this.writeLocalStorage();    
+  },
+
+  writeLocalStorage: function(){
+    var obj ={};
+    obj["playCount"]= this.get('playCount');
+    obj["upCount"]= this.get('upCount');
+    obj["downCount"] = this.get('downCount');
+
+    localStorage.setItem(this.get("url"), JSON.stringify(obj));
+
+  },
+
   play: function(){
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
     var playCount = this.get("playCount");
     playCount+=1;
     this.set('playCount',playCount);
+    this.writeLocalStorage();
 
-    localStorage.setItem(this.get("url"),this.get('playCount'))  ;
   },
   end: function(){
     this.trigger('removeQueue', this);
--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -3,11 +3,20 @@ var LibraryEntryView = Backbone.View.extend({
 
   tagName: 'tr',
 
-  template: _.template('<td>(<%= artist %>)</td><td><%= title %> (<%= playCount %>)</td>'),
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %> (<%= playCount %>)</td><td><span class="glyphicon glyphicon-thumbs-up">(<%= upCount %>)</span></td><td>  </td><td><span class="glyphicon glyphicon-thumbs-down">(<%= downCount %>)</span></td>'),
 
   events: {
-    'click': function() {
-      this.model.addQueue();
+    'click': function(ev) {
+      var cl = ev.target.classList[1];
+      if(cl && cl.indexOf('thumbs-up') > -1){
+        this.model.incrUpCount();
+      }
+      else if(cl && cl.indexOf('thumbs-down') > -1){
+        this.model.incrDownCount();
+        }
+      else{
+        this.model.addQueue();
+      }
       this.render();
     }
   },
